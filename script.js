9b80ec9266113489c50e5f58a25bf2859a2e0cb5
X-yahya <124516380+X-yahya@users.noreply.github.com>
2023-12-28 23:52:07 +0100

    added rgb button and its logic

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
 const grid = document.querySelector(".container");
 let cells = 16;
-let newValue = document.querySelector(".cellnum") ; 
-console.log(newValue.value) ;
 
+function createGrid(cells) {
+    for (let i = 0; i < cells; i++) {
+        let newGridCell = document.createElement("div");
+        newGridCell.classList.add("gridcell");
 
-for (let i = 0; i < cells; i++) {
-    let newgridcell = document.createElement("div");
-    newgridcell.classList.add("gridcell");
+        for (let j = 0; j < cells; j++) {
+            let newDiv = document.createElement("div");
+            newDiv.style.border = "1px solid black";
+            newDiv.classList.add("row");
+            newGridCell.appendChild(newDiv);
+        }
 
-    for (let j = 0; j < cells; j++) {
-        let newdiv = document.createElement("div");
-        newdiv.style.border = "1px solid black";
-        newdiv.classList.add("row");
-        newgridcell.appendChild(newdiv);
+        grid.appendChild(newGridCell);
     }
-
-    grid.appendChild(newgridcell);
 }
+createGrid(cells);
+const elements = document.querySelectorAll(".row");
+const clear = document.querySelector(".clear") ; 
+const white = document.querySelector(".white") ; 
+const black = document.querySelector(".black") ; 
+const rgb = document.querySelector(".Rgb") ; 
+let current_color = "";
+white.addEventListener("click",()=>{current_color= "white";}) ; 
+black.addEventListener("click",()=>{current_color="black";});  
+rgb.addEventListener("click",()=>{current_color = "rgb";});
+elements.forEach((elem) => {
+    elem.addEventListener("mouseover", () => {
+        if (current_color==="rgb")
+        {
+            elem.style.background = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})` ;
+        }
+        else{
+       elem.style.background  = current_color ; }
+    })
+    
+    clear.addEventListener("click",()=>{
+        elem.style.background = "white" ;
+    });
+
+    
+    
+});
+
+
+
